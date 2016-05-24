26fd0c8941dca03903c66e72381ccb2654774094
ashok.hottin <ashok.hottin@BDC3-L-54657ST.dir.svc.accenture.com>
2016-05-24 14:24:05 +0530

    2nd change

--- TestMyDevOps/src/com/ashok/TestmyDevOps.java
+++ TestMyDevOps/src/com/ashok/TestmyDevOps.java
@@ -5,6 +5,8 @@ public class TestmyDevOps {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello This is Ashok from DevOps");		
+		System.out.println("Hello This is Ashok from DevOps 123");
+		
 	}
 
 }
