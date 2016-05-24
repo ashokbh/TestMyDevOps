4b8c91ec8a21a9b540ba1ccedd9b6c9887b97855
ashok.hottin <ashok.hottin@BDC3-L-54657ST.dir.svc.accenture.com>
2016-05-24 14:03:06 +0530

    First Message

---
+++ TestMyDevOps/src/com/ashok/TestmyDevOps.java
@@ -0,0 +1,10 @@
+package com.ashok;
+
+public class TestmyDevOps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello This is Ashok from DevOps");
+	}
+
+}
